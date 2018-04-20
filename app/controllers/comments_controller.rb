48ede69a53bd72132a6e7e6534db9e653be3a2de
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-04-20 12:36:41 -0600

    Filled Out Posts & Comments Controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,42 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_comment, only: [:show, :update, :destory]
+
   def index
+    render json: Comment.order(created_at: :desc)
+  end
+
+  def create
+    comment = current_user.comments.create(comment_params)
+    if comment.save
+      render json: comment
+    else
+      render json: { errors: comment.errors.full_messages.join(',') }, status: 422
+    end
+  end
+
+  def show
+    render json: @comment
+  end
+
+  def update
+    if @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: { errors: comment.errors.full_messages.join(',') }, status: 422
+  end
+
+  def destory
+    @comment.destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name, :subject, :description, :liked)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id]) 
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,43 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post, only: [:show, :update, :destroy]
+
   def index
+    render json: Post.order(created_at: :desc)
+  end
+
+  def create
+    post = current_user.posts.create(post_params)
+    if post.save
+      render json: post
+    else
+      render json: { errors: post.errors.full_messages.join(',') }, status: 422
+    end
+  end
+
+  def show
+    render json: @post
+  end
+
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: { errors: post.errors.full_messages.join(',') }, status: 422
+    end
+  end
+
+  def destroy
+    @post.destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :subject, :description, :liked)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
