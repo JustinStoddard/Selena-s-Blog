4dd42914a345b7f8ed5c9f1a4c8ee9293b1fa7a5
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-04-25 13:49:37 -0600

    Adding layout to home page / First commit since devpont :)

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { 
+  Button, 
+  Card, 
+  Grid, 
+  Header, 
+  Image, 
+  Segment, 
+  Divider, 
+  Icon 
+} from 'semantic-ui-react';
 
 class Home extends React.Component {
   render() {
     return (
-      <Header as='h1' textAlign='center'>Home Component</Header>
-    );
+      <div>
+        <Header as='h1' textAlign='center'>Home Component</Header>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Card>
+                <Card.Content>
+                    <h1>This is the profile card</h1>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+
+            <Grid.Column width ={10}>
+              <Segment>
+                <h1>Header</h1>
+                <p>This is where either the posts or the home page will be placed, depending on where a user is on a page.</p>
+              </Segment>
+            </Grid.Column>
+
+            <Grid.Column width={3}> 
+              <Card>
+                <Card.Content>
+                    <h1>Post archive. Or ads ribbon.</h1>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    )
   }
 }
 
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -35,7 +35,10 @@ class NavBar extends Component {
       <div>
         <Menu pointing secondary>
           <Link to='/'>
-            <Menu.Item name='home' />
+            <Menu.Item name='Home' />
+          </Link>
+          <Link to='/blog'>
+            <Menu.Item name='Blog' />
           </Link>
           { this.rightNavs() }
         </Menu>
